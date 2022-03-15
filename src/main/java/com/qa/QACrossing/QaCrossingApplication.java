b18e48b3ab95ec7edce91bad94d34d628877bab0
callumcound <callumcound@protonmail.com>
2022-03-15 10:38:00 +0000

    CC: dev commit

---
+++ src/main/java/com/qa/QACrossing/QaCrossingApplication.java
@@ -0,0 +1,13 @@
+package com.qa.QACrossing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaCrossingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaCrossingApplication.class, args);
+	}
+
+}
