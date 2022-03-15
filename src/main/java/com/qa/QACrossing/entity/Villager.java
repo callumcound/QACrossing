875f566cb63289b804a62dca9249f3e3232fec2a
callumcound <callumcound@protonmail.com>
2022-03-15 11:10:28 +0000

    CC: feature1, corrected annotation for entity class

--- src/main/java/com/qa/QACrossing/entity/Villager.java
+++ src/main/java/com/qa/QACrossing/entity/Villager.java
@@ -3,10 +3,12 @@ package com.qa.QACrossing.entity;
 import java.util.Objects;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class Villager {
 
 	
