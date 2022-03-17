5b972422ea5a0e8b64bdb39d3b5049dc6ad18b76
callumcound <callumcound@protonmail.com>
2022-03-17 12:11:15 +0000

    CC: finished ControllerIntegrationTests

--- src/test/java/com/qa/QACrossing/rest/VillagerControllerIntegrationTest.java
+++ src/test/java/com/qa/QACrossing/rest/VillagerControllerIntegrationTest.java
@@ -1,29 +1,29 @@
 	package com.qa.QACrossing.rest;
 
-	import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-	import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-	import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-	import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-	import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+	import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-	import java.util.ArrayList;
-	import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 
-	import org.junit.jupiter.api.Test;
-	import org.springframework.beans.factory.annotation.Autowired;
-	import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-	import org.springframework.boot.test.context.SpringBootTest;
-	import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-	import org.springframework.http.MediaType;
-	import org.springframework.test.context.ActiveProfiles;
-	import org.springframework.test.context.jdbc.Sql;
-	import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
-	import org.springframework.test.web.servlet.MockMvc;
-	import org.springframework.test.web.servlet.RequestBuilder;
-	import org.springframework.test.web.servlet.ResultMatcher;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 
-	import com.fasterxml.jackson.databind.ObjectMapper;
-	import com.qa.QACrossing.entity.Villager;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qa.QACrossing.entity.Villager;
 
 	@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 	@AutoConfigureMockMvc
@@ -104,5 +104,10 @@
 					.contentType(MediaType.APPLICATION_JSON))
 					.andExpect(checkStatus)
 					.andExpect(checkBody);
+		}
+		
+		@Test
+		void testDelete() throws Exception {
+			this.mvc.perform(delete("/villager/delete/1")).andExpect(status().isNoContent());
 		}
 }
